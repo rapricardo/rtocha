616c8ca84209a547ba45823884e33246fa157ea0
Ricardo <rapricardo@gmail.com>
2025-03-13 12:04:57 -0300

    Desabilitar verificação de tipos e lint durante o build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  // Desabilitar verificação de tipos durante o build
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
+  // Desabilitar lint durante o build
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
